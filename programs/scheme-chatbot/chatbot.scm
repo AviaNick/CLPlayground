67acceb3d2196bf6589f9f05ccc7b310818dd716
AviaNick <48195230+AviaNick@users.noreply.github.com>
2021-06-30 23:25:04 -0400

    Update chatbot.scm

--- programs/scheme-chatbot/chatbot.scm
+++ programs/scheme-chatbot/chatbot.scm
@@ -1,1 +1,16 @@
+;This is NOT an AI!!!
+
+
+;Basic Hello World!
+;(display "Hello, World!") (newline)
+
+
+;Set name of Chatbot below 
+(define botName "")
+;User inputs their name below
+(display "Input your name: ") 
+(define name (read)) (newline)
+;Basic response from the Chatbot
+(display name) (display " is a good name!")(newline)
+(display "My name is ") (display botName) (display "!")(newline)
 
